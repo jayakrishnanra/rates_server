8bda6133dae362d7c012f4fdfa72b5ff7efbfa6d
jay <jayakrishnanra@gmail.com>
2016-11-26 10:11:35 +0000

    bug fix

--- src/main/java/com/eprotech/rates/server/Application.java
+++ src/main/java/com/eprotech/rates/server/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:63342")
+//@CrossOrigin(origins = "http://localhost:63342")
 @SpringBootApplication
 @RestController
 @RequestMapping(value = "/rs/fx/")
